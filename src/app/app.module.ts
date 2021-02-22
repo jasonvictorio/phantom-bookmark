c3a5da1273810093ea5d809b04412bbc4e462205
Jason Victorio <jgvictorio@gmail.com>
2021-02-22 15:12:04 +1300

    add reactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,13 +15,10 @@ import { BookmarksComponent } from './components/bookmarks/bookmarks.component';
     OverviewComponent,
     ResultsComponent,
     FormComponent,
-    BookmarksComponent
+    BookmarksComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
