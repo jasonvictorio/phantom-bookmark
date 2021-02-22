3840fcd27c42e3eb0263a434b947b514c21fb418
Jason Victorio <jgvictorio@gmail.com>
2021-02-22 14:28:59 +1300

    update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { OverviewComponent } from './pages/overview/overview.component';
+import { ResultsComponent } from './pages/results/results.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'overview', pathMatch: 'full' },
+  { path: 'overview', component: OverviewComponent },
+  { path: 'results', component: ResultsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
