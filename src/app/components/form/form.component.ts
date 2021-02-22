4eb871bdd86e4fd6cc3df6270d614bd58a1efc0f
Jason Victorio <jgvictorio@gmail.com>
2021-02-22 17:58:06 +1300

    add error message

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -11,6 +11,7 @@ import { Bookmark } from 'src/app/models/bookmark';
 export class FormComponent {
   @Output() submitForm = new EventEmitter<any>();
   URL_REGEX = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
+  errorMessage: string | null = null;
 
   bookmarkForm = this.fb.group({
     url: ['', [Validators.required, Validators.pattern(this.URL_REGEX)]],
@@ -20,9 +21,17 @@ export class FormComponent {
 
   onFormSubmit() {
     const newBookmark: Omit<Bookmark, 'id'> = this.bookmarkForm.value;
+    this.errorMessage = this.getErrorMessage();
 
     if (this.bookmarkForm.valid) {
       this.submitForm.emit(newBookmark);
     }
+  }
+
+  getErrorMessage() {
+    // basic error message deduction
+    if (this.bookmarkForm.value.url) return 'url is invalid';
+    if (!this.bookmarkForm.valid) return 'url is required';
+    return null;
   }
 }
