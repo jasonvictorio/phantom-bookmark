dcda4964cea02499cc1c88be645b53ca2e39244e
Jason Victorio <jgvictorio@gmail.com>
2021-02-22 14:19:24 +1300

    ng g component pages/results

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OverviewComponent } from './pages/overview/overview.component';
+import { ResultsComponent } from './pages/results/results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OverviewComponent
+    OverviewComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
