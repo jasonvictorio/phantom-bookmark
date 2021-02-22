06d2ef8d0a615420a415959203bed83faefa3b08
Jason Victorio <jgvictorio@gmail.com>
2021-02-22 15:14:00 +1300

    update form, append to overview

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Bookmark } from 'src/app/models/bookmark';
+
+import { BookmarkService } from 'src/app/services/bookmark.service';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
+// TODO: validation
 export class FormComponent implements OnInit {
+  bookmarkForm = this.fb.group({
+    url: [''],
+  });
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private bookmarkService: BookmarkService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit() {
+    const newBookmark: Omit<Bookmark, 'id'> = this.bookmarkForm.value;
+    this.bookmarkService.addBookmark(newBookmark).subscribe(() => {
+      this.bookmarkForm.reset();
+
+      // todo: use store, delegate service access to parent component?
+      this.bookmarkService.getBookmarks().subscribe((bookmarks) => {
+        console.log(bookmarks);
+      });
+    });
   }
-
 }
