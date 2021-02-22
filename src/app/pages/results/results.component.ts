5bee1f5169b825f12e192f0087007c0b030f9612
Jason Victorio <jgvictorio@gmail.com>
2021-02-22 16:35:20 +1300

    implement results page

--- src/app/pages/results/results.component.ts
+++ src/app/pages/results/results.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Bookmark } from 'src/app/models/bookmark';
+import { BookmarkService } from 'src/app/services/bookmark.service';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.scss']
+  styleUrls: ['./results.component.scss'],
 })
 export class ResultsComponent implements OnInit {
+  bookmark?: Bookmark;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private bookmarkService: BookmarkService
+  ) {}
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.bookmarkService.getBookmark(id).subscribe((bookmark) => {
+      this.bookmark = bookmark;
+    });
   }
-
 }
