b26bedba8fbf20cd803e32d1966c27f6922c7ca7
Jason Victorio <jgvictorio@gmail.com>
2021-02-22 15:01:13 +1300

    use service in bookmarks list and display bookmarks

--- src/app/components/bookmarks/bookmarks.component.ts
+++ src/app/components/bookmarks/bookmarks.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Bookmark } from 'src/app/models/bookmark';
+import { BookmarkService } from 'src/app/services/bookmark.service';
+
 @Component({
   selector: 'app-bookmarks',
   templateUrl: './bookmarks.component.html',
-  styleUrls: ['./bookmarks.component.scss']
+  styleUrls: ['./bookmarks.component.scss'],
 })
 export class BookmarksComponent implements OnInit {
+  bookmarks?: Bookmark[];
 
-  constructor() { }
+  constructor(private bookmarkService: BookmarkService) {}
 
   ngOnInit(): void {
+    this.getBookmarks();
   }
 
+  getBookmarks(): void {
+    this.bookmarkService.getBookmarks().subscribe((bookmarks) => {
+      this.bookmarks = bookmarks;
+    });
+  }
 }
