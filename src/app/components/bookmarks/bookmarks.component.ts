60da5dfd4813a16d8f70721390d75f962fd5a5fd
Jason Victorio <jgvictorio@gmail.com>
2021-02-22 16:57:48 +1300

    move getBookmarks from list to overview

--- src/app/components/bookmarks/bookmarks.component.ts
+++ src/app/components/bookmarks/bookmarks.component.ts
@@ -1,31 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { Bookmark } from 'src/app/models/bookmark';
-import { BookmarkService } from 'src/app/services/bookmark.service';
 
 @Component({
   selector: 'app-bookmarks',
   templateUrl: './bookmarks.component.html',
   styleUrls: ['./bookmarks.component.scss'],
 })
-export class BookmarksComponent implements OnInit {
-  bookmarks?: Bookmark[];
-
-  constructor(private bookmarkService: BookmarkService) {}
-
-  ngOnInit(): void {
-    this.getBookmarks();
-  }
-
-  getBookmarks(): void {
-    this.bookmarkService.getBookmarks().subscribe((bookmarks) => {
-      this.bookmarks = bookmarks;
-    });
-  }
-
-  deleteBookmark(bookmark: Bookmark): void {
-    this.bookmarkService.deleteBookmark(bookmark).subscribe(() => {
-      this.getBookmarks();
-    });
-  }
+export class BookmarksComponent {
+  @Input() bookmarks?: Bookmark[];
+  @Output() deleteBookmark = new EventEmitter<Bookmark>();
 }
--- src/app/pages/overview/overview.component.ts
+++ src/app/pages/overview/overview.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Bookmark } from 'src/app/models/bookmark';
+import { BookmarkService } from 'src/app/services/bookmark.service';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
-  styleUrls: ['./overview.component.scss']
+  styleUrls: ['./overview.component.scss'],
 })
 export class OverviewComponent implements OnInit {
+  bookmarks: Bookmark[] = [];
 
-  constructor() { }
+  constructor(private bookmarkService: BookmarkService) {}
 
   ngOnInit(): void {
+    this.getBookmarks();
+  }
+
+  getBookmarks(): void {
+    this.bookmarkService.getBookmarks().subscribe((bookmarks) => {
+      this.bookmarks = bookmarks;
+    });
+  }
+
+  onDeleteBookmark(bookmark: Bookmark): void {
+    this.bookmarkService.deleteBookmark(bookmark).subscribe(() => {
+      this.getBookmarks();
+    });
   }
 
 }
